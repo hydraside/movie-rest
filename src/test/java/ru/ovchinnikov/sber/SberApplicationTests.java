bb0850fcecdd7922ea6b90d4919f823db627fd6f
Данил Овчинников <dan9904@yandex.ru>
2023-06-06 19:45:06 +0500

    initial commit

---
+++ src/test/java/ru/ovchinnikov/sber/SberApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ovchinnikov.sber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
